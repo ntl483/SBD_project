88df0eaacea396fe8b9b9a2656903c7f2e9a740d
Natalia Jensko <natalia.jensko@gmail.com>
2021-11-25 21:53:08 +0100

    fixed bugs

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from data_producer import DataProducer
-from data_manager import DataManager
 from sorter import Sorter
+import sys
 
 
 def main():
-    dataProducer = DataProducer()
-    dataProducer.createFromInput()
-    s = Sorter()
-    s.sortFile(5)
 
+    dataProducer = DataProducer()
+    # dataProducer.createRandom(1000)
+    dataProducer.createRandom(100)
+    dataProducer.createRandom(500)
+    dataProducer.createRandom(900)
+    dataProducer.createRandom(1300)
+    s = Sorter()
+    s.sortFile(1)
+    s.sortFile(2)
+    s.sortFile(3)
+    s.sortFile(4)
+    #s.sortFile(5)
+    #s.sortFile(6)
+    #s.sortFile(7)
+    #s.sortFile(8)
+    #s.sortFile(9)
+    #s.sortFile(10)
 
 if __name__ == '__main__':
     main()
--- tape.py
+++ tape.py
@@ -1,8 +1,10 @@
 class Tape:
     content: list
+    ended: bool
 
     def __init__(self):
         self.content = []
+        self.ended = False
 
     def addToTape(self, val):
         self.content.append(val)
@@ -19,12 +21,22 @@ class Tape:
         return self.content[0]
 
     def popNext(self):
+        if len(self.content) == 1 or self.content[0] > self.content[1]:
+            self.ended = True
+            #print("run ended")
+        #print("popped:", self.content[0])
         self.content.pop(0)
 
     def sorted(self) -> bool:
         a = True
-        for i in range(1,len(self.content)):
+        for i in range(1, len(self.content)):
             if self.content[i-1] > self.content[i]:
                 a = False
                 break
         return a
+
+    def runEnded(self) -> bool:
+        return self.ended
+
+    def beginRun(self):
+        self.ended = False
